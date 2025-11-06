4b0115e55a92066d75f9ff15debad3c8d87cb064
kelv01 <kelvinnjugunanungari@gmail.com>
2025-11-06 19:47:36 +0300

    added code reformatted

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,7 +1,7 @@
-// This is the *correct* way to type an ES module
 import { RowID, RowElement } from './interface';
-declare module './crud.js' {
-  export function insertRow(row: RowElement): number; // Using number from hint
-  export function deleteRow(rowId: RowID): void;
-  export function updateRow(rowId: RowID, row: RowElement): RowID;
-}
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
+
+export { insertRow, deleteRow, updateRow };
--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,29 +1,22 @@
-// Import the types
+
 import { RowID, RowElement } from './interface';
 
-// Import from the .js file
+
 import * as CRUD from './crud.js';
 
-// --- Main Application ---
 
-// 1. Create a RowElement
 const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
 };
 
-// 2. Assign the inserted row's ID
-const newRowID: RowID = CRUD.insertRow(row);
-// Expected log: "Insert row {firstName: "Guillaume", lastName: "Salva"}" (rowID will be random)
 
-// 3. Create updatedRow
-// We use the spread operator to cleanly add the age
-// This line is now formatted to match the checker's expected string
+const newRowID: RowID = CRUD.insertRow(row);
+
 const updatedRow: RowElement = { ...row, age: 23 };
 
-// 4. Call updateRow and deleteRow
+
 CRUD.updateRow(newRowID, updatedRow);
-// Expected log: "Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}" (rowID will be random)
+
 
 CRUD.deleteRow(newRowID);
-// Expected log: "Delete row id 125" (rowID will be random)
