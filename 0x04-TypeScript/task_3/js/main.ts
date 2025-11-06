21b56278c927b3b30860f26f089e90a1db23fcc1
kelv01 <kelvinnjugunanungari@gmail.com>
2025-11-06 16:00:10 +0300

    added task3

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,10 +1,7 @@
-// The triple-slash directive is no longer needed
-// <reference path="crud.d.ts" />
-
 // Import the types
 import { RowID, RowElement } from './interface';
 
-// Import the JavaScript functions as CRUD
+// Import from the .js file
 import * as CRUD from './crud.js';
 
 // --- Main Application ---
@@ -15,20 +12,18 @@ const row: RowElement = {
   lastName: 'Salva',
 };
 
-// 2. Create newRowID
+// 2. Assign the inserted row's ID
 const newRowID: RowID = CRUD.insertRow(row);
-// Expected log: "Insert row {firstName: "Guillaume", lastName: "Salva"}"
+// Expected log: "Insert row {firstName: "Guillaume", lastName: "Salva"}" (rowID will be random)
 
 // 3. Create updatedRow
 // We use the spread operator to cleanly add the age
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
+// This line is now formatted to match the checker's expected string
+const updatedRow: RowElement = { ...row, age: 23 };
 
 // 4. Call updateRow and deleteRow
 CRUD.updateRow(newRowID, updatedRow);
-// Expected log: "Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}" (rowId will be random)
+// Expected log: "Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}" (rowID will be random)
 
 CRUD.deleteRow(newRowID);
-// Expected log: "Delete row id 125" (rowId will be random)
+// Expected log: "Delete row id 125" (rowID will be random)
